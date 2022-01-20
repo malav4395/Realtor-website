edede3dcc4107ae219a215ad813c33d69fa4e907
Malav Panchal <malav.panchal@molex.com>
2022-01-19 22:50:45 -0500

    4 kind of graphs

--- javascript/index.js
+++ javascript/index.js
@@ -1,21 +1,23 @@
 $(document).ready(function () {
   var values = cal_width_height();
-  console.log(values.width);
-  console.log(values.height);
   resize_stat_iframe(values.width, values.height);
 });
 
 $(window).resize(function () {
   var values = cal_width_height();
-  console.log(values.width);
-  console.log(values.height);
   resize_stat_iframe(values.width, values.height);
 });
 
 
-function resize_stat_iframe(w, h) {
-  $("#stat-iframe").width(w).height(h);
-  $("#stat-iframe").attr("src", "https://itso.stats.showingtime.com/infoserv/s-v1/rrpI-sEa?w=" + w + "&h=" + h);
+function resize_stat_iframe(w, h) { 
+  $("#sale-price").width(w).height(h);
+  $("#sale-price").attr("src", "https://itso.stats.showingtime.com/infoserv/s-v1/rrpI-sEa?w=" + w + "&h=" + h);
+  $("#home-for-sale").width(w).height(h);
+  $("#home-for-sale").attr("src", "https://itso.stats.showingtime.com/infoserv/s-v1/raMi-rOq?w=" + w + "&h=" + h);
+  $("#sold-stat").width(w).height(h);
+  $("#sold-stat").attr("src", "https://itso.stats.showingtime.com/infoserv/s-v1/raiv-hp3?w=" + w + "&h=" + h);
+  $("#days-on-market").width(w).height(h);
+  $("#days-on-market").attr("src", "https://itso.stats.showingtime.com/infoserv/s-v1/raJZ-lYT?w=" + w + "&h=" + h);
 }
 
 function cal_width_height() {
