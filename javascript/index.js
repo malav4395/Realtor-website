b300359a5f9ca694b9ccc7f9e855f9637a2b0bd4
Malav Panchal <malav.panchal@molex.com>
2022-01-19 20:58:00 -0500

    added service page

--- javascript/index.js
+++ javascript/index.js
@@ -5,6 +5,14 @@ $(document).ready(function () {
   resize_stat_iframe(values.width, values.height);
 });
 
+$(window).resize(function () {
+  var values = cal_width_height();
+  console.log(values.width);
+  console.log(values.height);
+  resize_stat_iframe(values.width, values.height);
+});
+
+
 function resize_stat_iframe(w, h) {
   $("#stat-iframe").width(w).height(h);
   $("#stat-iframe").attr("src", "https://itso.stats.showingtime.com/infoserv/s-v1/rrpI-sEa?w=" + w + "&h=" + h);
