9f1d4e5963c78f80689a22264244ec021f2a0221
Malav Panchal <malav.panchal@molex.com>
2022-01-19 23:01:31 -0500

    added KWAR page

--- javascript/index.js
+++ javascript/index.js
@@ -9,7 +9,7 @@ $(window).resize(function () {
 });
 
 
-function resize_stat_iframe(w, h) { 
+function resize_stat_iframe(w, h) {  
   $("#sale-price").width(w).height(h);
   $("#sale-price").attr("src", "https://itso.stats.showingtime.com/infoserv/s-v1/rrpI-sEa?w=" + w + "&h=" + h);
   $("#home-for-sale").width(w).height(h);
@@ -18,6 +18,7 @@ function resize_stat_iframe(w, h) {
   $("#sold-stat").attr("src", "https://itso.stats.showingtime.com/infoserv/s-v1/raiv-hp3?w=" + w + "&h=" + h);
   $("#days-on-market").width(w).height(h);
   $("#days-on-market").attr("src", "https://itso.stats.showingtime.com/infoserv/s-v1/raJZ-lYT?w=" + w + "&h=" + h);
+  $("#kwar-stat").width(w).height(h - 50);
 }
 
 function cal_width_height() {
